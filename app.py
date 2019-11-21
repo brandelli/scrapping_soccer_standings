e503fc1c954eb3b9eadef418744527a5a23c478d
Bruno Brandelli <bdbrandelli@hotmail.com>
2019-11-21 09:40:42 -0200

    adding heroku

--- app.py
+++ app.py
@@ -1,40 +1,12 @@
-from bs4 import BeautifulSoup as bs
-import requests
-import matplotlib.pyplot as plt
+from flask import Flask
+from flask_restful import Api
 
-def main():
-    URL = "https://fbref.com/en/comps/24/Serie-A-Stats"
-    page = requests.get(URL, timeout=5)
-    page_content = bs(page.content, "html.parser")
-    table_wrapper = page_content.find('div', attrs={'class': 'table_outer_container'})
-    head = get_head_data(table_wrapper)
-    table_rows = get_rows_data(table_wrapper)
-    print_table(head, table_rows)
-    
+from resources.todo import Todo
 
-def print_table(head, table_rows):
-    print(' '.join(head))
-    for row in table_rows:
-        print(' '.join(row))
+app = Flask(__name__)
+api = Api(app)
 
-def get_head_data(table_wrapper):
-    thead = table_wrapper.find('thead')
-    ths = thead.find_all('th')
-    return [val.text for val in ths]
+api.add_resource(Todo, "/todo/<int:id>")
 
-def get_rows_data(table_wrapper):
-    clean_rows = []
-    table = table_wrapper.find('tbody')
-    rows = table.find_all('tr')
-    for row in rows:
-        clean_tds = []
-        th = row.find('th').text
-        tds = row.find_all('td')
-        clean_tds.append(th)
-        clean_tds.extend([val.text for val in tds])
-        clean_rows.append(clean_tds)
-    
-    return clean_rows
-
-if  __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+  app.run()
