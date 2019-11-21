5ae0e2f6575ad4b42f34563d71ba46aefddb0c9d
Bruno Brandelli <bdbrandelli@hotmail.com>
2019-11-21 10:50:29 -0200

    Adding Soccer class

--- app.py
+++ app.py
@@ -2,12 +2,17 @@ from flask import Flask
 from flask_restful import Api
 
 from resources.todo import Todo, Todos
+from resources.soccer import Leagues, League, Standing
 
 app = Flask(__name__)
 api = Api(app)
 
 api.add_resource(Todo, "/todo/<int:id>")
 api.add_resource(Todos, "/todo/")
+
+api.add_resource(Leagues, "/leagues/")
+api.add_resource(League, "/leagues/<int:id>")
+api.add_resource(Standing, "/standing/<int:id>")
 
 
 if __name__ == "__main__":
---
+++ resources/soccer.py
@@ -0,0 +1,56 @@
+from bs4 import BeautifulSoup as bs
+import requests
+from flask_restful import Resource
+
+leagues = [
+    {
+        'id': 1,
+        'name' : 'Brasil',
+        'url': 'https://fbref.com/en/comps/24/Serie-A-Stats'
+    }
+]
+
+class Leagues(Resource):
+    def get(self):
+        return leagues, 200
+
+class League(Resource):
+    def get(self, id):
+        for league in leagues:
+            if(id == league["id"]):
+                return league, 200
+            return "Item not found for the id: {}".format(id), 404
+
+class Standing(Resource):
+    def get(self, id):
+        for league in leagues:
+            if(id == league["id"]):
+                url = "https://fbref.com/en/comps/24/Serie-A-Stats"
+                page = requests.get(url, timeout=5)
+                page_content = bs(page.content, "html.parser")
+                table_wrapper = page_content.find('div', attrs={'class': 'table_outer_container'})
+                response = {
+                    'table_head': self.get_head_data(table_wrapper),
+                    'table_rows': self.get_rows_data(table_wrapper)
+                }
+                return response, 200
+            return "Item not found for the id: {}".format(id), 404
+    
+    def get_head_data(self, table_wrapper):
+        thead = table_wrapper.find('thead')
+        ths = thead.find_all('th')
+        return [val.text for val in ths]
+
+    def get_rows_data(self, table_wrapper):
+        clean_rows = []
+        table = table_wrapper.find('tbody')
+        rows = table.find_all('tr')
+        for row in rows:
+            clean_tds = []
+            th = row.find('th').text
+            tds = row.find_all('td')
+            clean_tds.append(th)
+            clean_tds.extend([val.text for val in tds])
+            clean_rows.append(clean_tds)
+        
+        return clean_rows
